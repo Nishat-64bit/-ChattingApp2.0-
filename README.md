99e0b89d25930cb9622ab0babb22c8dc70ff4071
MahmudurNishat <rajbiroy23@gmail.com>
2024-05-25 18:16:35 +0600

    registration part functionality done

--- README.md
+++ README.md
@@ -8,9 +8,16 @@ in this project I have learned many things
 - useState Hook
 - Tailwaind Css 
 - FireBase
-- Many thing More 
 - handle event funtionality nesting onchange
 - handle eye funtionality
-- handle
-- form validation with regex
+- handle signup funtionality - 1.empty signup prevent 2.Error show on browser (span) 3.1ta input er error solve hole next input e gele 1st error chole jabe And Next ==>
+
+- form validation with 1.Email regex (kew jate invaid email na dite pare) 
+- very Important email regex // const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3}$/; // copy it 
+                    //  const regex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;// alernative
+
+- form validation with 2.password (kew jate weak password na dei )  
+- very important password ==>
+- regex  = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&.\-+*/])[A-Za-z\d@$!%*?&.\-+*/]{8,}$/  copy  
+      
 And Stay with me , Many thing will coming . This is a Big Project . 👁️ Keep Watching 👁️.
--- src/Component/Registration/Registration.jsx
+++ src/Component/Registration/Registration.jsx
@@ -3,18 +3,25 @@ import RegistrationImg from "../../assets/RegistrationImg.png";
 import { FaRegEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 
-
 const Registration = () => {
   //? ================ useState Hook ======================= //
   const [Email, SetEmail] = useState("");
   const [FullName, SetFullName] = useState("");
   const [Password, SetPassword] = useState("");
-  const [Eye,setEye]=useState(false); // use state value = Eye 
+  const [Eye, setEye] = useState(false); // use state value = Eye
   //console.log(Password);
   // SetEmail("Mahmudur Nishat")
   //console.log(Email);
   //console.log(FullName)
   //? ================ useState Hook ======================= //
+
+  //* ================ Error useState Hook ======================= //
+
+  const [EmailError, setEmailError] = useState("");
+  const [FullNameError, setFullNameError] = useState("");
+  const [PasswordError, setPasswordError] = useState("");
+
+  //* ================ Error useState Hook ======================= //
 
   //!  ================ prevent Reload ======================= //
   const handleSubmit = (event) => {
@@ -22,11 +29,14 @@ const Registration = () => {
   };
   //! ================ prevent Reload ======================= //
 
+  //? ======================= All Handle Functionality ========================= //
+
   // ======================= Handle Email Functionality ========================= //
   //  const HandleEmail = (event)=>{
   //     SetEmail(event.target.value);
   //  }
   // ======================= Handle Email Functionality ========================= //
+
   // ======================= Handle Name Functionality ========================= //
   // const HandleName = (event)=>{
   //     SetFullName(event.target.value)
@@ -38,13 +48,50 @@ const Registration = () => {
   //     SetPassword(event.target.value)
   // }
   // ======================= Handle Password Functionality ========================= //
-  //? ======================= Handle eye Functionality ========================= //
-      const handleEye=()=>{
-        setEye(!Eye)
-        // console.log(Eye);
-      }
-  //? ======================= Handle eye Functionality ========================= //
 
+  //? ======================= Handle eye Functionality start ========================= //
+  const handleEye = () => {
+    setEye(!Eye);
+    // console.log(Eye);
+  };
+  //? ======================= Handle eye Functionality end ========================= //
+
+  //*======================= Regex Email Functionality start ========================= //
+  const EmailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{3}$/;
+  const email = "nishatmahmudur89@gmail.com";
+  console.log(EmailRegex.test(email));
+
+  const PasswordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&.\-+*/])[A-Za-z\d@$!%*?&.\-+*/]{8,}$/;
+  const password1 = "SoftwareEngineer75@/com"
+  console.log(PasswordRegex.test(password1));
+  //*======================= Regex Email Functionality end ========================= //
+
+  //* ======================= Handle signup Functionality ========================= //
+  const handleSignup = () => {
+    if (!Email) {
+      setEmailError("Email Credential Missing Or Wrong ⚠️");
+    } else if (!EmailRegex.test(Email)) {
+      setEmailError("Email Credential Missing Or Wrong ⚠️");
+    } else if (!FullName) {
+      setEmailError("");
+      setFullNameError("Name is missing ⚠️");
+    } else if (!Password) {
+      setEmailError("");
+      setFullNameError("");
+      setPasswordError("Passwrod Credential Missing Or Wrong ⚠️");
+    }else if(!PasswordRegex.test(Password)){
+      setPasswordError("Password Credential Missing Or Wrong ⚠️");
+    } else {
+      setEmailError("");
+      setFullNameError("");
+      setPasswordError("");
+      //console.log("everything is oke");
+    }
+  };
+  // console.log(EmailError,FullNameError,PasswordError);
+  //*======================= Handle signup Functionality ========================= //
+
+  //? ======================= All handle Functionality ========================= //
   return (
     <>
       <div className=" flex justify-between items-center">
@@ -77,9 +124,18 @@ const Registration = () => {
                   // ======================= Handle Email Functionality ========================= //
                 />
               </div>
+
+              {/* email error start and its functionality start*/}
+              {EmailError && (
+                <span className="text-red-400 font-Nunito font-normal text-md">
+                  {EmailError}
+                </span>
+              )}
+              {/* email error end and its functionality end */}
+
               {/* email end */}
 
-              {/* name start */}
+              {/* Full name input start */}
               <div className="my-3">
                 <label
                   htmlFor="fullname"
@@ -99,7 +155,16 @@ const Registration = () => {
                   // ======================= Handle Name Functionality ========================= /
                 />
               </div>
-              {/* name end */}
+
+              {/* Full name error start and its functionality start*/}
+              {FullNameError && (
+                <span className="text-red-400 font-Nunito font-normal text-md">
+                  {FullNameError}
+                </span>
+              )}
+              {/* Full name error start and its functionality start*/}
+
+              {/* Full name input end */}
 
               {/* password start */}
               <div className="my-3 relative ">
@@ -110,7 +175,7 @@ const Registration = () => {
                   Password
                 </label>
                 <input
-                  type={Eye?"text":"password"}
+                  type={Eye ? "text" : "password"}
                   placeholder="1234@adfdsaf"
                   name="Password"
                   id="Password"
@@ -120,18 +185,26 @@ const Registration = () => {
                   onChange={(event) => SetPassword(event.target.value)}
                   // ======================= Handle Password Functionality ========================= //
                 />
-                
-              {/* eye button start */}
-              <div
-                className="absolute top-[53%] right-[3%] "
-                onClick={handleEye}
-              >
-                {Eye?<FaRegEye />:<FaEyeSlash />}
-                
-              </div>
-              {/* eye button End */}
+
+                {/* eye button start */}
+                <div
+                  className="absolute top-[53%] right-[3%] "
+                  // ======================= Handle Eye Functionality ========================= //
+                  onClick={handleEye} /// ! direct func call na kore setEye(!Eye) korte paren
+                  // ======================= Handle Eye Functionality ========================= //
+                >
+                  {Eye ? <FaRegEye /> : <FaEyeSlash />}
+                </div>
+                {/* eye button End */}
               </div>
 
+              {/* Password error start and its functionality start*/}
+              {PasswordError && (
+                <span className="text-red-400 font-Nunito font-normal text-md">
+                  {PasswordError}
+                </span>
+              )}
+              {/* Password error start and its functionality End*/}
 
               {/* password end */}
 
@@ -139,6 +212,9 @@ const Registration = () => {
               <button
                 type="submit"
                 className="w-full py-5 rounded-[86px] bg-btnColor text-white text-xl font-normal mt-3 font-Nunito"
+                // ======================= Handle login / signup Functionality ========================= //
+                onClick={handleSignup}
+                // ======================= Handle login / signup Functionality ========================= //
               >
                 Sign up
               </button>
@@ -146,14 +222,14 @@ const Registration = () => {
               {/* button end */}
             </form>
 
-            {/* sign in start */}
+            {/* sign in start & registration functionality */}
             <h3 className="text-center font-OpenSans font-normal text-[#03014C] text-sm mt-4">
               Already have an account?
               <span className="text-[#EA6C00] ml-1 cursor-pointer hover:underline hover:decoration-dark-blue ">
                 Sign in
               </span>
             </h3>
-            {/* sign in End */}
+            {/* sign in End & registration functionality  */}
           </div>
         </div>
         <div className=" w-1/2 h-fullvh">
